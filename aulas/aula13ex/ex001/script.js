70753ed852cd16d8d554b157e9e0250e9b65b32e
IsacBrenno <isacbrennobr@gmail.com>
2025-03-10 21:15:44 +0000

    finalizei o exercicio

--- aulas/aula13ex/ex001/script.js
+++ aulas/aula13ex/ex001/script.js
@@ -1,19 +1,29 @@
 function contar(){
+    let inicio = document.querySelector('input#txtinicio')
+    let fim = document.querySelector('input#txtfim')
+    let passo = document.querySelector('input#txtpasso')
+    let res = document.querySelector('div#res')
 
-    var txtinicio = document.querySelector('input#txtinicio');
-    var inicio = Number(txtinicio.value);
-
-    var txtfim = document.querySelector('input#txtfim');
-    var fim = Number(txtfim.value);
-
-    var txtpasso = document.querySelector('input#txtpasso')
-    var passo = Number(txtpasso.value)
-
-    var res = document.querySelector('div#res')
-    
-    while(inicio <= fim){
-        inicio = inicio + passo;
-        res.innerHTML = 'contando: '
-        res.innerHTML = inicio
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+        alert ('[ERRO], Faltam dados!')
+    } else{
+        res.innerHTML = 'Contando: '
+        let i = Number(inicio.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p <= 0){
+            alert('PASSO INVÁLIDO, Considerando PASSO 1')
+            p = 1
+        }
+        if (i < f){
+            for (let c = i; c <= f; c += p){
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        } else{
+            for (let c = i; c >= f; c -= p){
+                res.innerHTML += `${c} \u{1F449} `
+            }
+        } 
+        res.innerHTML += `\u{1F3C1}`
     }
 }
